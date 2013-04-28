349ee181e3121ca048c9f5bdaca6e50fab6f5b80
fatzhou <fatzhou@tencent.com>
2013-04-28 10:20:03 +0800

    回滚development_list.js

--- js/development_list.js
+++ js/development_list.js
@@ -2,20 +2,17 @@
 //	data.data.navPos = '7';
 //	global_obj.data.app_count = 194;
 //	var page_no = global_obj.data.page_no + 1; //页码
-if (!hdlogin)
+if(!hdlogin)
 {
 	$('#main').html(tmpl(this.tpl.login,global_obj.data));
-} else {
+}
+else
+{
 	var page_count = Math.ceil(global_obj.data.app_count/global_obj.data.page_size);  //页数
-	var kpage_count = Math.ceil(global_obj.data.kapp_count/global_obj.data.page_size);  //页数
-	if((page_count?page_count:1) < global_obj.data.page_no || (kpage_count?kpage_count:1) < global_obj.data.kpage_no){    //当前页码不能超过总页数
+	if(page_count < global_obj.data.page_no){    //当前页码不能超过总页数
 		alert("warning:The page_no excceeds the total page counts");
-	}
-	var pageBar 
+	}              
 
-	var cpage_no = global_obj.data.page_no;
-	var cpage_count = page_count;
-	var capp_count = global_obj.data.app_count;
 	var str = [
 		'.dotted{display:block;float:left;color:#346496;line-height:18px;}',
 	].join("");
@@ -103,39 +100,7 @@ if (!hdlogin)
 						'<%}%>',
 					'</ul>',
 	].join("");
-
-		tpl.applistul1 = [
-			   // <!--其他平台应用-->
-			   '<div id="otherapplist" class="applist2 hidden">',
-				   '<ul id="applistul1">',
-						'<%for(var i=0;i<kapps.length;i++){%>',    
-							'<%var kapp=kapps[i];%>',          
-							'<li>',
-							'<img src="<%=(kapp.app_icon_75?kapp.app_icon_75:"http://mat1.gtimg.com/app/opent/images/index/icon.jpg")%>" height="60" width="60" />',
-							'<dl>',
-								'<dt style="color:#346496"><%=kapp.app_name%></dt>' ,
-								'<dd><span>应用KEY：</span><label><%=kapp.app_id%></label></dd>',
-								'<dd><span>&nbsp;&nbsp;&nbsp;&nbsp;创建平台：</span><label> <%=kapp.create_plat%></label></dd>',
-								'<dd><span>&nbsp;&nbsp;&nbsp;&nbsp;已上线平台：</span><label><%=kapp.online_plat%></label></dd>',
-							'</dl>',
-							'<div align="right">',
-								'<%if(kapp.wb_had){%>',
-									'<a href="/development/appinfo?appid=<%=kapp.app_id%>">已接入到微博（查看）</a>',
-								'<%}else{%>',
-									'<a href="/development/appinfo?appid=<%=kapp.app_id%>?comfrom=<%=kapp.app_comefrom%> ">接入到微博 </a>',
-								'<%}%>',
-							'</div>',
-							'</li>',
-						'<%} if(!kapps||kapps.length==0){%>' ,
-							'<%if(showtype){%>',
-								'<li style="height:auto;">您的其他平台应用已全部导入到微博了</li>',
-							'<%}else{%>',
-								'<li style="height:auto;">您还没有在其他平台创建过应用</li>',
-							'<%}%>',
-						'<%}%>',
-					'</ul>',
-	].join("");
-
+	
 	this.tpl.development_list_app = [
 		//<!------------------------------------已创建的应用----------------------------------------->
 		'<ul id="appTab">',
@@ -160,7 +125,8 @@ if (!hdlogin)
 					'<label for="apptype3">站内应用</label>',
 				'</form>',
 				tpl.applistul,
-				tpl.applistul1,
+		//		'<div id="pagebar1"><%=pagelist1%></div>',
+		//		this.tpl.pageBar,
 			'</div>',  
 		'</div>'
 	].join("");
@@ -201,18 +167,18 @@ if (!hdlogin)
 	this.tpl.pageBar = [//翻页按钮
 		'<div id="pagebar">',
 			'<div class="pagebar">',
-				'<em title="共<%=app_cnt%>条">共<%=app_cnt%>条</em>',
-				'<%if(page_num > 1){%>',
-					'<a href="#<%=page_num-1%>" onclick="pageList(<%=page_num - 1%>);" class="page_prev">上一页</a>',
+				'<em title="共<%=app_count%>条">共<%=app_count%>条</em>',
+				'<%if(page_no > 1){%>',
+					'<a href="#<%=page_no-1%>" onclick="pageList(<%=page_no - 1%>);" class="page_prev">上一页</a>',
 				'<%}else{%>',
 					'<span class = "page_prev">上一页</span>',
 				'<%}%>',
 
 				'<%if(page_no <= 5 ){%>',//page_no之前的页全部显示
-					'<%if(page_num > page_cnt - 4){%>',
+					'<%if(page_no > page_count - 4){%>',
 						//显示所有页码
-						'<%for(var i = 1; i <= page_cnt; i++){%>',
-							'<%if (i != page_num) {%>',
+						'<%for(var i = 1; i <= page_count; i++){%>',
+							'<%if (i != page_no) {%>',
 								'<a href="#<%=i%>" onclick = pageList(<%=i%>)><%=i%></a>',
 							'<%}else{%>',
 								'<span><%=i%></span>',
@@ -220,7 +186,7 @@ if (!hdlogin)
 						'<%}%>',
 					'<%}else{%>',
 						//显示  1到curr curr+1 curr+2 curr+3... total
-						'<%for(var i = 1; i <= page_num + 3; i++){%>',
+						'<%for(var i = 1; i <= page_no + 3; i++){%>',
 							'<%if (i != page_no) {%>',
 								'<a href="#<%=i%>" onclick = pageList(<%=i%>)><%=i%></a>',
 							'<%}else{%>',
@@ -229,16 +195,16 @@ if (!hdlogin)
 						'<%}%>',
 						//'<span>...<span>',
 						'<em class="dotted">...</em>',
-						'<a href="#<%=page_cnt%>" onclick = pageList(<%=page_cnt%>)><%=page_cnt%></a>',
+						'<a href="#<%=page_count%>" onclick = pageList(<%=page_count%>)><%=page_count%></a>',
 					'<%}%>',
 				'<%}else{%>', 
-					'<%if(page_num > page_cnt - 5){%>',
+					'<%if(page_no > page_count - 5){%>',
 						//显示1...curr-3到total
 						'<a href="#1" onclick = pageList(1)>1</a>',
 						//'<span>...</span>',
 						'<em class="dotted">...</em>',
-						'<%for(var i = page_num - 3; i <= page_cnt; i++){%>',
-							'<%if (i != page_num) {%>',
+						'<%for(var i = page_no - 3; i <= page_count; i++){%>',
+							'<%if (i != page_no) {%>',
 								'<a href="#<%=i%>" onclick = pageList(<%=i%>)><%=i%></a>',
 							'<%}else{%>',
 								'<span><%=i%></span>',
@@ -249,8 +215,8 @@ if (!hdlogin)
 						'<a href="#1" onclick = pageList(1)>1</a>',
 					//	'<span>...</span>',
 						'<em class="dotted">...</em>',
-						'<%for(var i = page_num - 3; i <= page_num + 3; i++){%>',
-							'<%if (i != page_num) {%>',
+						'<%for(var i = page_no - 3; i <= page_no + 3; i++){%>',
+							'<%if (i != page_no) {%>',
 								'<a href="#<%=i%>" onclick = pageList(<%=i%>)><%=i%></a>',
 							'<%}else{%>',
 								'<span><%=i%></span>',
@@ -258,12 +224,12 @@ if (!hdlogin)
 						'<%}%>',						
 					//	'<span>...<span>',
 						'<em class="dotted">...</em>',
-						'<a href="#<%=page_cnt%>" onclick = pageList(<%=page_cnt%>)><%=page_cnt%></a>',
+						'<a href="#<%=page_count%>" onclick = pageList(<%=page_count%>)><%=page_count%></a>',
 					'<%}%>',
 				'<%}%>',
 
-				'<%if(page_num < page_cnt){%>',
-					'<a href="#<%=page_num+1%>" onclick="pageList(<%=page_num+1%> );" class="page_next">下一页</a>',
+				'<%if(page_no < page_count){%>',
+					'<a href="#<%=page_no+1%>" onclick="pageList(<%=page_no+1%> );" class="page_next">下一页</a>',
 				'<%}else{%>',
 					'<span class = "page_next">下一页</span>',
 				'<%}%>',
@@ -313,6 +279,8 @@ if (!hdlogin)
 	
 	$("#main").html(this.tmpl(this.tpl.development_list,global_obj.data));
 
+
+
 	var insiteAppAble=true;
 	var siteAppDisplayType = insiteAppAble?  16 : 0; 
 	var displayAppType = 45;//+siteAppDisplayType;
@@ -350,15 +318,15 @@ if (!hdlogin)
 			var apptypeUrl = "/interfaceserver?action=common_query&business_type=ajax_applist&appTypes="+displayAppType;
 			AjaxPageList(apptypeUrl);
 		})
-/*
+
 		//pageBar
-		if(global_obj.data.page_cnt == 1){
+		if(page_count == 1){
 			$('#pagebar').css('display','none');
 		}
 		else{
 			$('#pagebar').css('display','block');
 		}    
-		*/
+		
 		// TAB
 		var content = $("#appContent").children("div.applist2");
 		$("#appTab a").click(function () {
@@ -382,7 +350,8 @@ if (!hdlogin)
 			var ajaxpageListUrl ="/development/indexajaxapplist/"+page+'/'+displayAppType+'/'+"?d="+(new Date().getTime());
 		}*/
 		global_obj.data.page_no = page;
-		ajaxpageListUrl = "http://open_test.t.qq.com/pipes/interfaceserver?action=common_query&business_type=ajax_applist&page="+page;
+	//	var action = "common_query", business_type="ajax_applist", cookie_uin="1935849399";
+		ajaxpageListUrl = "http://open_test.t.qq.com/pipes/interfaceserver"+"?action="+action+"&business_type="+business_type+"&cookie_uin="+cookie_uin+"&page="+page;
 		AjaxPageList(ajaxpageListUrl);
 	} 
 	/**
@@ -390,37 +359,31 @@ if (!hdlogin)
 	 */
 	function pageList1(page){ 
 		global_obj.data.kpage_no = page;
-		ajaxpageListUrl = "http://open_test.t.qq.com/pipes/interfaceserver?action=common_query&business_type=ajax_applist&kpage="+page;
+		var ajaxpageListUrl ="http://open_test.t.qq.com/pipes/interfaceserver"+"?action="+action+"&business_type="+business_type+"&cookie_uin="+cookie_uin+"&page="+page;
 		AjaxPageList(ajaxpageListUrl);
 	} 
 		
 	function AjaxPageList(ajaxpageListUrl){ 
-		console.log("AjaxPageList");
 		$.ajax({
 			  url: ajaxpageListUrl,
 			  dataType: "json",
 			  cache: false,
 			  success: function(ResposeData){ 
+//				  if(ResposeData.uin == hdlogin){
+				  console.log(ResposeData);			
+				  console.log(ResposeData.data);
+				  console.log(ResposeData.uin);
+				  ResposeData.data.kapps = ResposeData.data.kapps || {};
+				  ResposeData.data.apps = ResposeData.data.apps || {};
 				  if(ResposeData.data.uin == hdlogin){
-				  	if ($('#otherapplist').hasClass('hidden')) {
-				  		console.log(1);
-						ResposeData.data.page_num = global_obj.data.page_no;
-						ResposeData.data.page_cnt = page_count;
-						ResponseData.data.app_cnt = global_obj.data.app_count;
-						$('#applistul').html(tmpl(tpl.applistul, ResposeData.data));
-						$('#pagebar').html(tmpl(tpl.pageBar, ResposeData.data));
-					}else if (!ResposeData.data.kapps) {
-						console.log(2);
-						ResposeData.data.page_num = global_obj.data.kpage_no;
-						ResposeData.data.page_cnt = kpage_count;
-						ResponseData.data.app_cnt = global_obj.data.kapp_count;
-						$('#applistul1').html(tmpl(tpl.applistul1, ResposeData.data));
-						$('#pagebar').html(tmpl(tpl.pageBar, ResposeData.data));
-					}
-					console.log(3);
+				/*	$('#applistul').html(ResposeData.html);
+					$('#pagebar').html(ResposeData.pagestr); 
+					$('#applistul1').html(ResposeData.html1);
+					$('#pagebar1').html(ResposeData.pagestr1);*/
+					$('#applistul').html(tmpl(tpl.applistul, ResposeData.data));
+					$('#pagebar').html(tmpl(tpl.pageBar, ResposeData.data));
 				  }else{
-				  	console.log(4);
-					location.href="/development/";
+				//	location.href="/development/";
 				  }
 			  }
 		})
