a6f664a2542a29ced57343c8088617f2c57ad44e
w <w>
2013-05-20 16:25:34 +0800

    修改连接

--- js/websites_comment_explain_include.js
+++ js/websites_comment_explain_include.js
@@ -141,7 +141,7 @@ function formSubmit() {
 				return;
 			}
 			if (d.ret === 0 && d.data && d.data.id) {
-				location.href = '/development/compinfo/' + d.data.id;
+				location.href = '/development/compinfo?comp_id=' + d.data.id;
 			} else {
 				loginWin.alert({
 					"title": "获取代码失败！",
